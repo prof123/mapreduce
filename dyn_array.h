8c6c54f694318fac5b2edcae1e061524e218a6bf
Prateek Sharma <prateeksharma@BG-222.local>
2010-03-12 23:34:20 +0530

    dynamic array implementation.

--- dyn_array.h
+++ dyn_array.h
@@ -1,6 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-struct KV_ARRAY {
-	keyy_t key;
-	size_t size;
-	void* val_array[];
-};
+/**
+ * Dynamic array implementation
+ */
+
+#ifndef ELEMENT
+typedef char ELEMENT ;
+#endif
+
+struct dynarry_struct {
+	int count ;
+	int max ;
+	size_t elem_size ;
+	ELEMENT* DATA ;
+	
+}; 
+
+typedef struct dynarry_struct* DYNARRAY ;
+
+/*****************************************************************************/
+DYNARRAY new_array (size_t elem_size) ;
+
+int insert_into (DYNARRAY array , ELEMENT e) ;
+
+
+
+
