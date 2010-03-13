787e481b88186deafce8ea90cab01bf20faf653f
Prateek Sharma <prateeksharma@BG-222.local>
2010-03-14 01:03:47 +0530

    .

--- MAP.h
+++ MAP.h
@@ -5,10 +5,16 @@
 typedef char* map_key_t ;
 typedef char* map_val_t ;
 
-struct map_return_struct {
+struct MAP_keyval {
 	map_key_t key ;
 	map_val_t value ;
-}
+};
+
+struct MAP_out {
+	int count ;  	//number of key-value pairs in the dynamic array
+	DYNARRAY data ; //actual mapped list
+};
+	
 
 
 
